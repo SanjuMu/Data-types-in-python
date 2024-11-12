ae880d6ed7d791ad56e699d1c4b58452c13c6be3
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-11 20:14:41 -0500

    casting

---
+++ casing.py
@@ -0,0 +1,28 @@
+name = "sanju"
+age =13
+is_student = True
+weight = 34.2
+
+
+print("Name:" , name)
+print("Data type of Name is", type(name))
+
+
+
+print("Age:" , age)
+print("Data type of Age is", type(age))
+
+
+
+print("is-student:" , is_student)
+print("Data type of isstudent is", type(is_student))
+
+print("Weight:", weight)
+print("Data Type of Weight is", type(weight))
+
+age= str(age)
+print(age )
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
